8275b7d016855cb0e91750d07cc2e9bc3612f0e2
Muhammad Awais <awaistopper20@gmail.com>
2022-08-24 17:04:38 +0500

    beforeEach hook.

--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -2,17 +2,18 @@ import { mount } from "@vue/test-utils";
 import TodoApp from "@/components/TodoApp.vue";
 
 describe("TodoApp.vue", () => {
-    test("should render todo text", () => {
-        const wrapper = mount(TodoApp);
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(TodoApp);
+    })
 
+    test("should render todo text", () => {
         const todo = wrapper.get("[data-test='todo']");
 
         expect(todo.text()).toBe("Learn vue testing.");
     });
 
     test("should add new todo", async () => {
-        const wrapper = mount(TodoApp);
-
         expect(wrapper.findAll("[data-test='todo']")).toHaveLength(1);
 
         await wrapper.get("[data-test='new-todo']").setValue("New todo");
@@ -23,8 +24,6 @@ describe("TodoApp.vue", () => {
 
 
     test("should be able to complete todo", async () => {
-        const wrapper = mount(TodoApp);
-
         await wrapper.get("[data-test='todo-checkbox']").setValue(true);
 
         expect(wrapper.get("[data-test='todo'").classes()).toContain("completed");
